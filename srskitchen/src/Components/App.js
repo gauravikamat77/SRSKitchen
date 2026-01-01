0196cec9fcf261c3df3870bbb30323b3339f74ef
Gauravi Kamat <gauravikamat77@gmail.com>
2026-01-01 16:20:04 +0530

    Force home page render

--- srskitchen/src/Components/App.js
+++ srskitchen/src/Components/App.js
@@ -7,16 +7,17 @@ import Home from './Home';
 import Contact from './Contact';
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <Routes>
-         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  );
+   return <Home />;
+  // return (
+  //   <div>
+  //     <Header/>
+  //     <Routes>
+  //        <Route path="/" element={<Home />} />
+  //       <Route path="/menu" element={<Menu />} />
+  //       <Route path="/contact" element={<Contact />} />
+  //     </Routes>
+  //   </div>
+  // );
 }
 
 export default App;
--- srskitchen/src/index.js
+++ srskitchen/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
-import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 
 
