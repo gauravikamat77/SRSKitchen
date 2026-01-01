09300dc8ff73518db425a01ab124026900f0f818
Gauravi Kamat <gauravikamat77@gmail.com>
2026-01-01 15:58:35 +0530

    Fix routing for GitHub Pages

--- srskitchen/src/index.js
+++ srskitchen/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+<HashRouter>
+  <App />
+</HashRouter>
 );
 
 
